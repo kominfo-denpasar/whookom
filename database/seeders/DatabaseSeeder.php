3b4dc19bb594737dc11cf474c8c37f3b1ea469ff
zeneight <thezeneight@gmail.com>
2025-02-09 12:28:00 +0800

    feat: edit seeder dan menjalankan file seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Database\Eloquent\Model;
+use Database\Seeders\PermissionsTableSeeder;
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\ConnectRelationshipsSeeder;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,6 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Model::unguard();
+
+            $this->call(PermissionsTableSeeder::class);
+            $this->call(RolesTableSeeder::class);
+            $this->call(ConnectRelationshipsSeeder::class);
+            //$this->call('UsersTableSeeder');
+
+        Model::reguard();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
