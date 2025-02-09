6eea14208d6cebb4b9a13906090bccc380ea7b0a
zeneight <thezeneight@gmail.com>
2025-02-09 17:30:12 +0800

    feat: filter menu untuk masing-masing role

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,60 +6,53 @@
     </a>
 </li>
 
+@role('admin', true)
+    <li class="nav-item">
+        <a href="{{ route('psikologs.index') }}" class="nav-link {{ Request::is('psikologs*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Psikolog</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="{{ route('masyarakats.index') }}" class="nav-link {{ Request::is('masyarakats*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Masyarakat</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="{{ route('jadwals.index') }}" class="nav-link {{ Request::is('jadwals*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Jadwals</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="{{ route('masalahs.index') }}" class="nav-link {{ Request::is('masalahs*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Masalahs</p>
+        </a>
+    </li>
+@endrole
 
-<li class="nav-item">
-    <a href="{{ route('psikologs.index') }}" class="nav-link {{ Request::is('psikologs*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Psikolog</p>
-    </a>
-</li>
+@level(3)
+    <li class="nav-item">
+        <a href="{{ route('keluhans.index') }}" class="nav-link {{ Request::is('keluhans*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Keluhans</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="{{ route('konselings.index') }}" class="nav-link {{ Request::is('konselings*') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Konselings</p>
+        </a>
+    </li>
+@endrole
 
-<li class="nav-item">
-    <a href="{{ route('masyarakats.index') }}" class="nav-link {{ Request::is('masyarakats*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Masyarakat</p>
-    </a>
-</li>
 
-<li class="nav-item">
+<!-- <li class="nav-item">
     <a href="{{ route('dassPertanyaans.index') }}" class="nav-link {{ Request::is('dassPertanyaans*') ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Dass Pertanyaans</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('dasshasils.index') }}" class="nav-link {{ Request::is('dasshasils*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Dasshasils</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('keluhans.index') }}" class="nav-link {{ Request::is('keluhans*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Keluhans</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('jadwals.index') }}" class="nav-link {{ Request::is('jadwals*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Jadwals</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('masalahs.index') }}" class="nav-link {{ Request::is('masalahs*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Masalahs</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('konselings.index') }}" class="nav-link {{ Request::is('konselings*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Konselings</p>
     </a>
 </li>
 
@@ -69,3 +62,10 @@
         <p>Konseling Masalahs</p>
     </a>
 </li>
+
+<li class="nav-item">
+    <a href="{{ route('dasshasils.index') }}" class="nav-link {{ Request::is('dasshasils*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Dasshasils</p>
+    </a>
+</li> -->
