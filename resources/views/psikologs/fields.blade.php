ac3bf98b63d7033f2f838e4eaffbb68f41172f84
zeneight <thezeneight@gmail.com>
2025-02-13 08:11:19 +0800

    feat: halaman CRUD psikolog

--- resources/views/psikologs/fields.blade.php
+++ resources/views/psikologs/fields.blade.php
@@ -1,23 +1,149 @@
-<!-- Nik Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('nik', 'Nik:') !!}
-    {!! Form::text('nik', null, ['class' => 'form-control', 'required']) !!}
-</div>
-
 <!-- Nama Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('nama', 'Nama:') !!}
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('nama', 'Nama Lengkap') !!}
     {!! Form::text('nama', null, ['class' => 'form-control', 'required']) !!}
 </div>
 
 <!-- Hp Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('hp', 'Hp:') !!}
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('hp', 'Nomor HP (Whatsapp)') !!}
     {!! Form::text('hp', null, ['class' => 'form-control', 'required']) !!}
 </div>
 
+<!-- Field -->
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('nik', 'NIK') !!}
+    {!! Form::text('nik', null, ['class' => 'form-control', 'required']) !!}
+    <small>Nomor Induk Kependudukan</small>
+</div>
+
+<!-- Field -->
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('kta', 'KTA') !!}
+    {!! Form::text('kta', null, ['class' => 'form-control', 'required']) !!}
+    <small>Kartu Tanda Anggota</small>
+</div>
+
+<!-- Field -->
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('sipp', 'Nomor SIPP') !!}
+    {!! Form::text('kta', null, ['class' => 'form-control', 'required']) !!}
+    <small>Surat Izin Praktik Psikologi</small>
+</div>
+
+<!-- Field -->
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('alamat_rumah', 'Alamat Rumah') !!}
+    {!! Form::textarea('alamat_rumah', null, ['class' => 'form-control', 'required']) !!}
+</div>
+
+<!-- Field -->
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('alamat_praktek', 'Alamat Praktek') !!}
+    {!! Form::textarea('alamat_praktek', null, ['class' => 'form-control', 'required']) !!}
+</div>
+
+<div class="col-md-12">
+    <hr>
+</div>
+
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('kec_id', 'Kecamatan Alamat Praktek') !!}
+    <select name="kec_id" id="kec_id" class="form-control" required="">
+        <option value="">Pilih</option>
+    </select>
+</div>
+
+<div class="form-group col-sm-8 offset-sm-2">
+    {!! Form::label('desa_id', 'Desa Alamat Praktek') !!}
+    <select name="desa_id" id="desa_id" class="form-control" required="">
+        <option value="">Pilih</option>
+    </select>
+</div>
+
+<div class="col-md-12">
+    <hr>
+</div>
+
 <!-- User Id Field -->
-<div class="form-group col-sm-6">
+<!-- <div class="form-group col-sm-6">
     {!! Form::label('user_id', 'User Id:') !!}
     {!! Form::number('user_id', null, ['class' => 'form-control']) !!}
-</div>
+</div> -->
+
+@push('page_scripts')
+
+<script type="text/javascript">
+	const fetchAllTodos = () => {
+	return fetch("//www.emsifa.com/api-wilayah-indonesia/api/districts/5171.json")
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok.');
+			}
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data);
+			return data;
+		})
+		.catch((error) => {
+			return error;
+		});
+	};
+
+	// Ambil tag id container
+	const containerDisplay = document.getElementById('kec_id')
+
+	// Komponen Card untuk render semua data
+	const cardComponent = (id, title) => {
+		// Buat Card
+		const data = `
+			<option value="${id}">${title}</option>
+		`
+		// Tambahkan kedalam elemen container yang sudah kita definisikan sebelumnya
+		containerDisplay.insertAdjacentHTML('afterbegin', data)
+	}
+
+	function render() {
+		fetchAllTodos()
+			.then((response) => {
+				response.forEach(result => {
+					cardComponent(result.id, result.name);
+				});
+			})
+			.catch((error) => {
+				console.error('Error rendering data:', error);
+			});
+	}
+
+	render();
+
+	document.querySelector('#kec_id').addEventListener('change', function() {
+		const opsi = document.querySelector('#kec_id').value;
+
+		const url = `//www.emsifa.com/api-wilayah-indonesia/api/villages/${opsi}.json`;
+
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok.');
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				const containerDisplay = document.getElementById('desa_id');
+				containerDisplay.innerHTML = "<option value=''>Pilih</option>";
+				data.forEach(result => {
+					const data = `
+						<option value="${result.id}">${result.name}</option>
+					`
+					containerDisplay.insertAdjacentHTML('afterbegin', data)
+				});
+			})
+			.catch((error) => {
+				return error;
+			});
+	});
+</script>
+@endpush
