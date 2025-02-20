420189902420a6ce8855199a6841547d3f1af5ed
zeneight <thezeneight@gmail.com>
2025-02-21 05:02:03 +0800

    feat: dinasmis halaman laporan

--- resources/views/backend/laporan_detail.blade.php
+++ resources/views/backend/laporan_detail.blade.php
@@ -9,8 +9,8 @@
 			</div>
 			<div class="col-sm-6">
 			<ol class="breadcrumb float-sm-right">
-				<li class="breadcrumb-item"><a href="#">Dashboard Psikolog</a></li>
-				<li class="breadcrumb-item"><a href="#">Detail Konseling</a></li>
+				<li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard Psikolog</a></li>
+				<li class="breadcrumb-item"><a href="{{route('backend.konseling', $data->keluhan_id)}}">Detail Konseling</a></li>
 				<li class="breadcrumb-item active">Laporan Detail Konseling</li>
 			</ol>
 			</div>
@@ -23,53 +23,49 @@
 			<div class="row">
 				<div class="col-12">
 					<div class="callout callout-info">
-					<h5><i class="fas fa-info"></i> Note:</h5>
-					This page has been enhanced for printing. Click the print button at the bottom of the invoice to test.
+						<h5><i class="fas fa-info"></i> Perhatian:</h5>
+						Berikut adalah laporan detail konseling. Gunakan tombol export untuk mendownload dalam format PDF.
 					</div>
+				</div>
+				<!-- .col-12 -->
 
-
+				<div class="offset-2 col-8">
 					<!-- Main content -->
 					<div class="invoice p-3 mb-3">
 					<!-- title row -->
 					<div class="row">
 						<div class="col-12">
 						<h4>
-							<i class="fas fa-globe"></i> AdminLTE, Inc.
-							<small class="float-right">Date: 2/10/2014</small>
+							<i class="fas fa-globe"></i> Psychological Record.
+							<small class="float-right">Tanggal: {{ \Carbon\Carbon::parse($data->tanggalnya)->format('d/m/Y')}}</small>
 						</h4>
 						</div>
 						<!-- /.col -->
 					</div>
 					<!-- info row -->
 					<div class="row invoice-info">
-						<div class="col-sm-4 invoice-col">
-						From
+						<div class="col-sm-3 invoice-col">
 						<address>
-							<strong>Admin, Inc.</strong><br>
-							795 Folsom Ave, Suite 600<br>
-							San Francisco, CA 94107<br>
-							Phone: (804) 123-5432<br>
-							Email: info@almasaeedstudio.com
+							<strong>Nama Pemeriksa<br>
+							Nama Klien<br>
+							Jenis Kelamin<br>
+							Tanggal Lahir/Usia<br>
+							Pendidikan Terakhir<br>
+							Pekerjaan<br>
+							Kecamatan<br></strong>	
 						</address>
 						</div>
 						<!-- /.col -->
 						<div class="col-sm-4 invoice-col">
-						To
 						<address>
-							<strong>John Doe</strong><br>
-							795 Folsom Ave, Suite 600<br>
-							San Francisco, CA 94107<br>
-							Phone: (555) 539-1037<br>
-							Email: john.doe@example.com
+							:&nbsp; {{$data->psikolog_nama}}<br>
+							:&nbsp; {{$data->nama}}<br>
+							:&nbsp; {{$data->jk}}<br>
+							:&nbsp; {{\Carbon\Carbon::parse($data->tgl_lahir)->format('d/m/Y')}}<br>
+							:&nbsp; {{$data->pendidikan}}<br>
+							:&nbsp; {{$data->pekerjaan}}<br>
+							:&nbsp; {{App\Http\Controllers\PsikologController::kec($data->kec_id)}}<br>
 						</address>
-						</div>
-						<!-- /.col -->
-						<div class="col-sm-4 invoice-col">
-						<b>Invoice #007612</b><br>
-						<br>
-						<b>Order ID:</b> 4F3S8J<br>
-						<b>Payment Due:</b> 2/22/2014<br>
-						<b>Account:</b> 968-34567
 						</div>
 						<!-- /.col -->
 					</div>
@@ -78,47 +74,155 @@
 					<!-- Table row -->
 					<div class="row">
 						<div class="col-12 table-responsive">
-						<table class="table table-striped">
-							<thead>
-							<tr>
-							<th>Qty</th>
-							<th>Product</th>
-							<th>Serial #</th>
-							<th>Description</th>
-							<th>Subtotal</th>
-							</tr>
-							</thead>
-							<tbody>
-							<tr>
-							<td>1</td>
-							<td>Call of Duty</td>
-							<td>455-981-221</td>
-							<td>El snort testosterone trophy driving gloves handsome</td>
-							<td>$64.50</td>
-							</tr>
-							<tr>
-							<td>1</td>
-							<td>Need for Speed IV</td>
-							<td>247-925-726</td>
-							<td>Wes Anderson umami biodiesel</td>
-							<td>$50.00</td>
-							</tr>
-							<tr>
-							<td>1</td>
-							<td>Monsters DVD</td>
-							<td>735-845-642</td>
-							<td>Terry Richardson helvetica tousled street art master</td>
-							<td>$10.70</td>
-							</tr>
-							<tr>
-							<td>1</td>
-							<td>Grown Ups Blue Ray</td>
-							<td>422-568-642</td>
-							<td>Tousled lomo letterpress</td>
-							<td>$25.99</td>
-							</tr>
-							</tbody>
-						</table>
+							<table class="table">
+								<thead>
+									<tr>
+										<th>A. Keluhan</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											{{$data->keluhan}}
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+
+					<!-- Table row -->
+					<div class="row">
+						<div class="col-12 table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th colspan="2">B. Masalah yang dialami</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td width="50%">
+											[list masalah]
+										</td>
+										<td>
+											[list deskripsi masalah]
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+
+					<!-- Table row -->
+					<div class="row">
+						<div class="col-12 table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>C. Data Anamnesis</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											[data anamnesis]
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+
+					<!-- Table row -->
+					<div class="row">
+						<div class="col-12 table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>D. Asesmen dan Hasil Pemeriksaan</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											{{$konseling->hasil}}
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+
+					<!-- Table row -->
+					<div class="row">
+						<div class="col-12 table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>E. Kesimpulan</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											{{$konseling->kesimpulan}}
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+
+					<!-- Table row -->
+					<div class="row">
+						<div class="col-12 table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>F. Saran</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											{{$konseling->saran}}
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.col -->
+					</div>
+					<!-- /.row -->
+
+					<!-- Table row -->
+					<div class="row">
+						<div class="col-12 table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>G. Dokumentasi</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											<img class="img-fluid" src="{{ asset('uploads/berkas_pendukung/'.$konseling->berkas_pendukung)}}" alt="Dokumentasi">
+										</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 						<!-- /.col -->
 					</div>
@@ -127,42 +231,14 @@
 					<div class="row">
 						<!-- accepted payments column -->
 						<div class="col-6">
-						<p class="lead">Payment Methods:</p>
-						<img src="../../dist/img/credit/visa.png" alt="Visa">
-						<img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-						<img src="../../dist/img/credit/american-express.png" alt="American Express">
-						<img src="../../dist/img/credit/paypal2.png" alt="Paypal">
-
-						<p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-							Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-							plugg
-							dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-						</p>
 						</div>
 						<!-- /.col -->
-						<div class="col-6">
-						<p class="lead">Amount Due 2/22/2014</p>
-
-						<div class="table-responsive">
-							<table class="table">
-							<tbody><tr>
-								<th style="width:50%">Subtotal:</th>
-								<td>$250.30</td>
-							</tr>
-							<tr>
-								<th>Tax (9.3%)</th>
-								<td>$10.34</td>
-							</tr>
-							<tr>
-								<th>Shipping:</th>
-								<td>$5.80</td>
-							</tr>
-							<tr>
-								<th>Total:</th>
-								<td>$265.24</td>
-							</tr>
-							</tbody></table>
-						</div>
+						<div class="col-6 text-center mb-5">
+							<p class="text-center" style="margin-bottom: 2px;font-weight:bold">Denpasar, {{\Carbon\Carbon::parse()->format('d M Y')}}</p>
+							<p class="text-center well well-sm shadow-none" style="margin-top: 75px;margin-bottom: 0px;font-weight:bold">
+								<u>({{$data->psikolog_nama}})</u>
+							</p>
+							<span class="text-muted">{{$data->sipp}}</span>
 						</div>
 						<!-- /.col -->
 					</div>
@@ -171,13 +247,10 @@
 					<!-- this row will not appear when printing -->
 					<div class="row no-print">
 						<div class="col-12">
-						<a href="invoice-print.html" rel="noopener" target="_blank" class="btn btn-default"><i class="fas fa-print"></i> Print</a>
-						<button type="button" class="btn btn-success float-right"><i class="far fa-credit-card"></i> Submit
-							Payment
-						</button>
-						<button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
-							<i class="fas fa-download"></i> Generate PDF
-						</button>
+							<a href="{{url('admin/home-psikologi/print')}}" rel="noopener" target="_blank" class="btn btn-default float-right"><i class="fas fa-print"></i> Print</a>
+							<button type="button" class="btn btn-secondary float-right" style="margin-right: 5px;">
+								<i class="fas fa-download"></i> PDF
+							</button>
 						</div>
 					</div>
 					</div>
