221bb782bf2982a763f57f745d3092db2d542c34
zeneight <thezeneight@gmail.com>
2025-03-05 11:38:16 +0800

    miscs: halaman beranda di halaman selesai

--- resources/views/front/evaluasi_selesai.blade.php
+++ resources/views/front/evaluasi_selesai.blade.php
@@ -30,6 +30,19 @@
 				</div>
 			</div>
 
+			<div class="space-y-2 text-center">
+				<a href="{{url('/')}}" class="mt-4 mb-2 group relative inline-block focus:outline-none focus:ring">
+						<span
+							class="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"
+						></span>
+
+						<span
+							class="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold tracking-widest text-black group-active:text-opacity-75">
+							Halaman Beranda
+						</span>
+					</a>
+				</div>
+
 			<!-- <ul class="max-w-2xl mx-auto my-5 divide-y shadow shadow-blue-600 rounded-xl">
 				<li>
 					<details class="group" open>
