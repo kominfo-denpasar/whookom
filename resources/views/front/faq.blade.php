4d2965a5b2995a7b37c4d287a34a91e3ff43cb9e
zeneight <thezeneight@gmail.com>
2025-03-06 10:49:26 +0800

    miscs: tambah halaman faq

---
+++ resources/views/front/faq.blade.php
@@ -0,0 +1,30 @@
+@extends('front.layouts.app')
+
+@section('content')
+<div class="relative p-4 mt-10">
+    <div class="max-w-3xl mx-auto">
+        <div
+            class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+            <div class="p-6">
+
+                <a href="{{ url('/') }}" class="text-indigo-600 hover:text-gray-700 transition duration-500 ease-in-out text-sm">
+                    Beranda
+                </a>
+                <a>> <span class="text-gray-900">Frequent Asked Question (FAQ)</span></a>
+
+                <h1 href="#" class="text-gray-900 font-bold text-4xl">Frequent Asked Question (FAQ)</h1>
+                <div class="py-5 text-sm font-regular text-gray-900 flex">
+                    <span class="ml-1">Berikut adalah list pertanyaan yang sering ditanyakan</span></a>
+                </div>
+                <hr>
+
+                <p class="text-base leading-8 my-5">
+                    {!! $faq->value !!}
+                </p>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
